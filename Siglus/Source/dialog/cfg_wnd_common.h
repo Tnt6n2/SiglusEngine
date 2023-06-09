0b7a30050eb09350f7634993e06375b84d5ce46b
Kawaii <17507206+nyanrukoowo@users.noreply.github.com>
2023-06-09 21:14:47 +0800

    y'all need jesus

---
+++ Siglus/Source/dialog/cfg_wnd_common.h
@@ -0,0 +1,39 @@
+
+#define		DLG_CM_SPACE			6
+
+#define		DLG_CM_SOLO_PDG_X		12
+#define		DLG_CM_SOLO_PDG_T		12
+#define		DLG_CM_SOLO_PDG_B		36
+
+#define		DLG_CM_BASE_PDG_X		12
+#define		DLG_CM_BASE_PDG_T		12
+#define		DLG_CM_BASE_PDG_B		12
+
+#define		DLG_CM_GROUP_PDG_X		12
+#define		DLG_CM_GROUP_PDG_T		16
+#define		DLG_CM_GROUP_PDG_B		8
+
+#define		DLG_CM_STC_H			16
+
+#define		DLG_CM_BTN_H			24
+
+#define		DLG_CM_BTN_INIT_W		102
+#define		DLG_CM_BTN_INIT_H		DLG_CM_BTN_H
+
+#define		DLG_CM_BTN_CLOSE_W		80
+#define		DLG_CM_BTN_CLOSE_H		DLG_CM_BTN_H
+
+#define		DLG_CM_CHKBOX_ADD_WIDTH		20
+#define		DLG_CM_CHKBOX_H				16
+
+#define		DLG_CM_RADIO_ADD_WIDTH		20
+#define		DLG_CM_RADIO_H				16
+
+#define		DLG_CM_SLIDER_WIDTH		48
+#define		DLG_CM_SLIDER_HEIGHT	32
+
+#define		DLG_CM_SCROLL_WIDTH		18
+
+int			Gf_get_volume_group_width();
+int			Gf_get_volume_group_num();
+int			Gf_get_bgmfade_group_width();
